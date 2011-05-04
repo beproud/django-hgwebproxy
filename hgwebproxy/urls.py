b15067aa2f60d4273f51eecac2e2cc9b8d891c4a
Ian Lewis <IanMLewis@gmail.com>
2011-05-05 00:07:39 +0900

    Removed duplicated repo_detail view from urls.py

--- hgwebproxy/urls.py
+++ hgwebproxy/urls.py
@@ -12,8 +12,3 @@ if settings.DEBUG:
     urlpatterns += patterns('hgwebproxy.views',
         url('^static/(?P<file_name>.+)$', 'static_file', name='repo_static_file'),
     )
-
-urlpatterns += patterns('hgwebproxy.views', 
-    url('^(?P<pattern>.+)', 'repo_detail', name='repo_detail'),
-)
-
