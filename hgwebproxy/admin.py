73c18833ae8c5a9bf60bfd0e84c0e446bb0843ca
Ian Lewis <IanMLewis@gmail.com>
2011-05-05 00:32:33 +0900

    import the unquote function from django's admin module

--- hgwebproxy/admin.py
+++ hgwebproxy/admin.py
@@ -1,6 +1,7 @@
 __docformat__ = "restructedtext"
 
 from django.utils.translation import ugettext as _
+from django.contrib.admin.util import unquote
 from django.contrib import admin
 from django import forms
 
@@ -143,7 +144,7 @@ class RepositoryAdmin(admin.ModelAdmin):
     def change_view(self, request, object_id, extra_context=None):
         # Redirect the user to the public repository view if
         # The user does not have permission to change the repository.
-        obj = self.get_object(request, unquote(object_id))
+        obj = self.get_object(request, object_id)
         if (self.has_view_permission(request, obj) and not
                 self.has_change_permission(request, obj)):
             return redirect(obj)
@@ -181,94 +182,5 @@ class RepositoryAdmin(admin.ModelAdmin):
         if obj:
             has_perm = has_perm and obj.has_delete_permission(request.user)
         return has_perm
-
-    def explore(self, request, id, *args):
-        opts = self.model._meta
-        app_label = opts.app_label
-
-        response = HttpResponse()
-        repo = get_object_or_404(Repository, pk=id)
-        if not self.has_view_permission(request, repo):
-            raise PermissionDenied
-
-        hgr = HgRequestWrapper(
-            request,
-            response,
-            script_name=repo.get_admin_explore_url(),
-        )
-        hgr.set_user(request.user.username)
-
-        """
-        Run the `hgwebdir` method from Mercurial directly, with
-        our incognito request wrapper, output will be buffered. Wrapped
-        in a try:except: since `hgweb` *can* crash.
-
-        Mercurial now sends the content through as a generator.
-        We need to iterate over the output in order to get all of the content
-        """
-
-        template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-        hgserve = hgweb(str(repo.location))
-
-        hgserve.reponame = repo.slug
-        # TODO: A more flexible way to get the default template path of mercurial
-        hgserve.templatepath = (template_dir, '/usr/share/mercurial/templates')
-
-        hgserve.repo.ui.setconfig('web', 'description', smart_str(repo.description))
-        hgserve.repo.ui.setconfig('web', 'name', smart_str(hgserve.reponame))
-        # encode('utf-8') FIX "decoding Unicode is not supported" exception on mercurial
-        hgserve.repo.ui.setconfig('web', 'contact', smart_str(repo.owner.get_full_name()))
-        hgserve.repo.ui.setconfig('web', 'allow_archive', repo.allow_archive)
-        hgserve.repo.ui.setconfig('web', 'style', 'monoblue_plain')
-        hgserve.repo.ui.setconfig('web', 'baseurl', repo.get_admin_explore_url())
-        hgserve.repo.ui.setconfig('web', 'staticurl', STATIC_URL)
-
-        try:
-            response.write(''.join([each for each in hgserve.run_wsgi(hgr)]))
-        except KeyError:
-            response['content-type'] = 'text/html'
-            response.write('hgweb crashed.')
-            # if hgweb crashed you can do what you like, throw a 404 or continue on
-            # hgweb tends to throw these on invalid requests..?
-            pass
-
-        context = {
-            'app_label': app_label,
-            'opts': opts,
-            'has_change_permission': self.has_change_permission(request, repo),
-            'original': repo,
-            'content': response.content,
-            'reponame' : hgserve.reponame,
-            'static_url' : STATIC_URL,
-            'slugpath': request.path.replace(repo.get_admin_explore_url(), '') or 'summary',
-            'is_root': request.path == repo.get_admin_explore_url(),
-        }
-
-        """
-        In cases of downloading raw files or tarballs, we don't want to
-        pass the output to our template, so instead we just return it as-is.
-        """
-        if response.has_header('content-type'):
-            if not response['content-type'].startswith("text/html"):
-                return response
-
-        """
-        Otherwise, send the content on to the template, for any kind
-        of custom layout you want around it.
-        """
-        return render_to_response("admin/hgwebproxy/repository/explore.html", context, RequestContext(request))
-
-
-    def get_urls(self):
-        from django.conf.urls.defaults import patterns, url 
-        urls = super(RepositoryAdmin, self).get_urls()
-        my_urls = patterns('',
-            url(
-                r'(?P<id>\d+)/explore',
-                self.admin_site.admin_view(self.explore),
-                name='hgwebproxy_repository_explore',
-            ),
-        )
-        return my_urls + urls
 
 admin.site.register(Repository, RepositoryAdmin)
